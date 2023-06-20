20703db23fe683e99ae3606a7702525e2faedaf5
vmfas <124852156+vmfas@users.noreply.github.com>
2023-06-20 18:22:56 -0300

    att

--- portfolio/assets/js/api.js
+++ portfolio/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/vmfas/javascript/main/Projetos/portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/vmfas/projetosWeb/main/portfolio/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
