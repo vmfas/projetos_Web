d15293fa6e1b5ee65c6383eef32befca869eb35d
vmfas <124852156+vmfas@users.noreply.github.com>
2023-08-02 13:27:09 -0300

    att relogio

--- relogio-dinamico/src/js/index.js
+++ relogio-dinamico/src/js/index.js
@@ -9,10 +9,13 @@ const relogio = setInterval(function time() {
     let s = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
+
     if (min < 10) min = '0' + min;
+
     if (s < 10) s = '0' + s;
 
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = s;
-});
+
+})
